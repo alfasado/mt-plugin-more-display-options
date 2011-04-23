0a9ef1e0f243f27eeb422f268e8858ae0cb54b9d
Alfasado Inc <webmaster@alfasado.jp>
2011-04-23 14:37:19 +0900

    Update for MT5.1.

--- plugins/MoreDisplayOptions/lib/MoreDisplayOptions/Plugin.pm
+++ plugins/MoreDisplayOptions/lib/MoreDisplayOptions/Plugin.pm
@@ -31,6 +31,7 @@ sub prefs_option_param {
 
 sub prefs_option_source {
     my ( $cb, $app, $tmpl ) = @_;
+    $$tmpl =~ s/disabled="disabled"//g;
     my $prefs = 'entry_prefs';
     my $object_type = 'entry';
     if ( $app->param( '_type' ) eq 'page' ) {
@@ -41,52 +42,61 @@ sub prefs_option_source {
     my @prefs = split( /,/, $entry_prefs );
     my $show_title = 1 if grep( /^title$/, @prefs );
     my $show_text = 1 if grep( /^text$/, @prefs );
-    $$tmpl =~ s/disabled="disabled"//g;
     my $search = quotemeta( '<mt:setvarblock name="html_head" append="1">' );
-    my $head = <<'HEAD';
+    my $css = '';
+    if (! $show_title ) {
+        $css .= '<style type="text/css">#title-field{display:none;}</style>';
+    }
+    if (! $show_text ) {
+        $css .= '<style type="text/css">#text-field{display:none;}</style>';
+    }
+    $$tmpl =~ s/($search)/$1$css/;
+    if ( MT->version_id =~ /^5\.0/ ) {
+        my $head = <<'HEAD';
     <script type="text/javascript">
-        function setTitleField( cb ) {
-            if ( cb.checked ) {
-                getByID( 'title-field' ).style.display='block';
+        function setTitleField(cb) {
+            if (cb.checked) {
+                jQuery('#title-field').css('display','block');
             } else {
-                getByID( 'title-field' ).style.display='none';
+                jQuery('#title-field').css('display','none');
             }
         }
         function setTextField( cb ) {
-            if ( cb.checked ) {
-                getByID( 'text-field' ).style.display='block';
+            if (cb.checked) {
+                jQuery('#text-field').css('display','block');
             } else {
-                getByID( 'text-field' ).style.display='none';
+                jQuery('#text-field').css('display','none');
             }
         }
     </script>
 HEAD
-    if (! $show_title ) {
-        $head .= <<'HEAD';
-        <style type="text/css">
-            #title-field { display:none; }
-        </style>
+        $$tmpl =~ s/($search)/$1$head/;
+    } else {
+        my $iliad = quotemeta( "var data = [ 'title', 'text' ];" );
+        my $iliad_new = <<'HEAD';
+          var data = [];
+          if (jQuery('#title-field').css('display') != 'none') {
+              data.push('title');
+          }
+          if (jQuery('#text-field').css('display') != 'none') {
+              data.push('text');
+          }
 HEAD
+        $$tmpl =~ s/$iliad/$iliad_new/;
     }
-    if (! $show_text ) {
-        $head .= <<'HEAD';
-        <style type="text/css">
-            #text-field { display:none; }
-        </style>
-HEAD
-    }
-    $$tmpl =~ s/($search)/$1$head/;
     return 1;
 }
 
 sub prefs_option_output {
     my ( $cb, $app, $tmpl ) = @_;
-    my $search = quotemeta( '<input type="checkbox" name="custom_prefs" id="custom-prefs-title" value="title" onclick="setCustomFields();' );
-    my $replace = '<input type="checkbox" name="custom_prefs" id="custom-prefs-title" value="title" onclick="setTitleField(this);"';
-    $$tmpl =~ s/$search/$replace/g;
-    $search = quotemeta( '<input type="checkbox" name="custom_prefs" id="custom-prefs-text" value="text" onclick="setCustomFields();' );
-    $replace = '<input type="checkbox" name="custom_prefs" id="custom-prefs-text" value="text" onclick="setTextField(this);"';
-    $$tmpl =~ s/$search/$replace/g;
+    if ( MT->version_id =~ /^5\.0/ ) {
+        my $search = quotemeta( '<input type="checkbox" name="custom_prefs" id="custom-prefs-title" value="title" onclick="setCustomFields();' );
+        my $replace = '<input type="checkbox" name="custom_prefs" id="custom-prefs-title" value="title" onclick="setTitleField(this);"';
+        $$tmpl =~ s/$search/$replace/g;
+        $search = quotemeta( '<input type="checkbox" name="custom_prefs" id="custom-prefs-text" value="text" onclick="setCustomFields();' );
+        $replace = '<input type="checkbox" name="custom_prefs" id="custom-prefs-text" value="text" onclick="setTextField(this);"';
+        $$tmpl =~ s/$search/$replace/g;
+    }
     return 1;
 }
 
